878c46528540d563871530a7dcbc617041d0bb7b
Tyler Kim <tylerjsook@gmail.com>
2025-11-13 17:44:45 -0800

    dynamic api endpoints

--- bootcamp-milestone-2/app/blog/page.tsx
+++ bootcamp-milestone-2/app/blog/page.tsx
@@ -2,9 +2,29 @@ import BlogPreview from "@/components/blogPreview";
 import connectDB from "@/database/db";
 import Blog from "@/database/blogSchema";
 
-export default async function BlogPage() {
+async function getBlogs() {
   await connectDB();
-  const blogs = await Blog.find().sort({ date: -1 }).orFail();
+
+  try {
+    const blogs = await Blog.find().sort({ date: -1 }).orFail();
+    return blogs;
+  } catch (err) {
+    return null;
+  }
+}
+
+export default async function BlogPage() {
+  const blogs = await getBlogs();
+
+  if (!blogs) {
+    return (
+      <main className="main-bio">
+        <div id="blog-container">
+          <p>No blogs found or an error occurred.</p>
+        </div>
+      </main>
+    );
+  }
 
   return (
     <main className="main-bio">
